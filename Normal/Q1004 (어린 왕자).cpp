ccf76e43320afe0d7a074515358ba904debc4f97
tinyfrog <tinyfrog12@hanmail.net>
2018-09-22 12:00:57 +0900

    Add files via upload

---
+++ Normal/Q1004 (어린 왕자).cpp
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+typedef struct _st {
+	int x, y;
+	_st() {
+		x = 0, y = 0;
+	}
+} Point;
+
+int main(void)
+{
+	int i, j, cases, answer, planet, rad;
+	Point start, end, mid;
+	scanf("%d", &cases);
+	for (i = 0; i < cases; i++) {
+		answer = 0;
+		scanf("%d %d %d %d", &start.x, &start.y, &end.x, &end.y);
+		scanf("%d", &planet);
+		for (j = 0; j < planet; j++) {
+			scanf("%d %d %d", &mid.x, &mid.y, &rad);
+
+			if ((rad * rad) >(start.x - mid.x) * (start.x - mid.x) + (start.y - mid.y) * (start.y - mid.y) || (rad * rad) > (end.x - mid.x) * (end.x - mid.x) + (end.y - mid.y) * (end.y - mid.y)) ++answer;
+			if ((rad * rad) > (start.x - mid.x) * (start.x - mid.x) + (start.y - mid.y) * (start.y - mid.y) && (rad * rad) > (end.x - mid.x) * (end.x - mid.x) + (end.y - mid.y) * (end.y - mid.y)) --answer;
+		} // 한 원안에 시작점, 도착점 둘다 있는 경우
+		printf("%d\n", answer);
+	}
+	return 0;
+}
