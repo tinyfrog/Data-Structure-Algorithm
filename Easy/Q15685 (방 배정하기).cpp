a9c9b1f0dbec41834af610c7b9300da08caf99c7
tinyfrog <tinyfrog12@hanmail.net>
2018-10-13 21:19:43 +0900

    Add files via upload

---
+++ Easy/Q15685 (방 배정하기).cpp
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j, k, a, b, c, n;
+	bool flag = false;
+	scanf("%d %d %d %d", &a, &b, &c, &n);
+
+	for (i = 0; i * a <= n; i++) {
+		for (j = 0; j * b <= n; j++) {
+			for (k = 0; k * c <= n; k++) {
+				if (i * a + j * b + k * c == n) {
+					flag = true;
+					break;
+				}
+			}
+			if (flag) break;
+		}
+		if (flag) break;
+	}
+
+	if (flag) printf("1\n");
+	else printf("0\n");
+	return 0;
+}
+
+// 문제 : https://www.acmicpc.net/problem/14697
+// 브루트 포스
